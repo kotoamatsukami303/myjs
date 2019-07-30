bd7ec950515c2e92735202cfa21bf2f31eb1f143
kotoamatsukami303 <53462573+kotoamatsukami303@users.noreply.github.com>
2019-07-30 18:57:22 +0800

    Update eeee.js

--- eeee.js
+++ eeee.js
@@ -7,9 +7,14 @@ function startpost(json){
   for (var i = 0; i < numposts; i++){
     var entry = json.feed.entry[i];
     var posttitle = entry.title.$t;
-    var sortBy = "titleasc";
     var posturl;
-        
+    var nonSortedArray = ['hi', 'yo', 'whatup', 'bye', 'lol'];
+    var reverseSortedArray = nonSortedArray.sort(function (a, b) {
+      if (a > b) return -1;
+      else if (a < b) return 1;
+      return 0;
+    });
+    
     if (i == json.feed.entry.length) break;
     
     for (var k = 0; k < entry.link.length; k++){
